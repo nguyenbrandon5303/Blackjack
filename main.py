2309d6a8ecd2b72ddaa4b9f1edb96ffd4ce7cb3a
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2019-06-21 16:07:15 +0300

    Update main.py
    Switched from using lists to dictionaries for creating the deck. This required an small changes to how we would get a card from the deck. 
    Implemented 3 values within each card: display name, suit, value. Display name is the name displayed in the game (ace, 2, 3,... king). Suit is the 4 suits (e.g. Diamond). Value is the hidden value of the card (royal names are valued as 10).

--- main.py
+++ main.py
@@ -2,25 +2,50 @@
 # Blackjack game
 import random
 
-class Deck():
+class deck():
     # creates a deck with # of decks (deck_size) times 52
     @staticmethod
     def create_deck(deck_size):
+        
+        actual_deck = {}
+        
+        for deck in range(deck_size * 52):
+            # converts deck into a number between 1 and 13, inclusive
+            value = int(deck/4) + 1
+            
+            # for values 11,12,13 (Jack, Queen, King) gives that key a value of 10
+            if(value > 10):
+                value = 10
+            
+            # Designates the suit of the card
+            if(deck % 4 == 0):
+                suit = "Diamond"
+            elif(deck % 4 == 1):
+                suit = "Clubs"
+            elif(deck % 4 == 2):
+                suit = "Hearts"
+            elif(deck % 4 == 3):
+                suit = "Spades"
 
-        actual_deck = {1: ["Ace of Spades", 11]}
-
-        for single_deck in range(51*deck_size):
-            actual_deck.append(single_deck)
-
-        # shuffles the whole deck
-        random.shuffle(actual_deck)
+            # Displays the card number (ace, one, two, ..., king)
+            card = str(int(deck/4) + 1)
+            
+            if(card == "1"):
+                card = "Ace"
+            elif(card == "11"):
+                card = "Jack"
+            elif(card == "12"):
+                card = "Queen"
+            elif(card == "13"):
+                card = "King"
+            actual_deck[deck] = [card, suit , value]
+        
         return actual_deck
-
+    
     # prints the whole deck
     @staticmethod
     def print_deck(actual_deck):
         print(actual_deck)
-
 
 class Dealer():
     # creates the dealer with condition of hitting until 17, standing otherwise
@@ -35,19 +60,18 @@ class Player():
     pass
 
 # plays the game by going through each player + dealer, deals the cards, etc.
-class Main():
+class main():
     print("Welcome to Blackjack")
     
     # User inputs # of decks but is converted to a string, ord converts string to integer
     temp_decks = input("Please enter the number of decks you would like to use: ")
     num_decks = int(temp_decks)
-    trial = Deck()
-    trial_deck = trial.create_deck(num_decks)
+    trial = deck()
+    trial_deck = trial.create_deck(num_decks).copy()
     trial.print_deck(trial_deck)
-    print('Your have been dealt a:')
-    print(trial_deck.pop())
-    print('and a')
-    print(trial_deck.pop())
-
+    
+    # returns and removes a random card from trial_deck
+    print(trial_deck.pop(random.choice(list(trial_deck.keys()))))
+    print(trial_deck.pop(random.choice(list(trial_deck.keys()))))
 
 Main()
