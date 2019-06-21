9a7bc4fa2e2d3c34b9ec81ff3ed613a9173a3909
Ryan McWay <rmcway@dons.usfca.edu>
2019-06-21 15:58:12 +0300

    Just some grammatical errors/syntax

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Deck():
     # creates a deck with # of decks (deck_size) times 52
     @staticmethod
     def create_deck(deck_size):
-        
+
         actual_deck = {1: ["Ace of Spades", 11]}
 
         for single_deck in range(51*deck_size):
@@ -15,7 +15,7 @@ class Deck():
         # shuffles the whole deck
         random.shuffle(actual_deck)
         return actual_deck
-    
+
     # prints the whole deck
     @staticmethod
     def print_deck(actual_deck):
@@ -23,6 +23,14 @@ class Deck():
 
 class Dealer():
     # creates the dealer with condition of hitting until 17, standing otherwise
+    pass
+
+class Chickens():
+    # creates cpu players to play with player: Ester, Petra and Elinor
+    pass
+
+class Player():
+    # creates the actions for the player, attributes
     pass
 
 # plays the game by going through each player + dealer, deals the cards, etc.
