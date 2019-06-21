df23e00d0cd054225c78250fb091d2ad8650c3fb
Ryan McWay <rmcway@dons.usfca.edu>
2019-06-21 11:02:45 +0300

    Initial commit

---
+++ main-new.py
@@ -0,0 +1,30 @@
+
+# class Main():
+
+class Deck():
+    standard_deck = {"Ace of Spades": 11, "Ace of Hearts": 11, "Ace of Diamonds": 11, "Ace of Clubs": 11,
+            "King of Spades": 10, "King of Hearts": 10,"King of Diamonds": 10,"King of Clubs": 10,
+            "Queen of Spades": 10,"Queen of Hearts": 10,"Queen of Diamonds": 10,"Queen of Clubs": 10,
+            "Jack of Spades": 10,"Jack of Hearts": 10,"Jack of Diamonds": 10,"Jack of Clubs": 10,
+            "10 of Spades": 10,"10 of Hearts": 10,"10 of Diamonds": 10,"10 of Clubs": 10,
+            "9 of Spades": 9,"9 of Hearts": 9,"9 of Diamonds": 9,"9 of Clubs": 9,
+            "8 of Spades": 8,"8 of Hearts": 8,"8 of Diamonds": 8,"8 of Clubs": 8,
+            "7 of Spades": 7,"7 of Hearts": 7,"7 of Diamonds": 7,"7 of Clubs": 7,
+            "6 of Spades": 6,"6 of Hearts": 6,"6 of Diamonds": 6,"6 of Clubs": 6,
+            "5 of Spades": 5, "5 of Hearts": 5,"5 of Diamonds": 5,"5 of Clubs": 5,
+            "4 of Spades": 4,"4 of Hearts": 4,"4 of Diamonds": 4,"4 of Clubs": 4,
+            "3 of Spades": 3,"3 of Hearts": 3,"3 of Diamonds": 3,"3 of Clubs": 3,
+            "2 of Spades": 2,"2 of Hearts": 2,"2 of Diamonds": 2,"2 of Clubs": 2,
+            }
+    print(standard_deck )
+#     def deck_num:
+#
+#         pick how many decks you want
+#     def choosen_deck:
+#         standard_deck*deck_num
+# class Dealer():
+#
+# class Player():
+#
+
+Deck()
