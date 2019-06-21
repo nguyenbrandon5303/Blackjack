29570455464f08e8198b776303904e94be86ee5a
Ryan McWay <rmcway@dons.usfca.edu>
2019-06-21 14:56:56 +0300

    Just some grammatical errors/syntax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 # Blackjack game
 import random
 
@@ -8,12 +7,11 @@ class Deck():
     @staticmethod
     def create_deck(deck_size):
         
-        actual_deck = []
-        
-        for multi_deck in range(deck_size):
-            for single_deck in range(52):
-                actual_deck.append(single_deck+1)
-        
+        actual_deck = {1: ["Ace of Spades", 11]}
+
+        for single_deck in range(51*deck_size):
+            actual_deck.append(single_deck)
+
         # shuffles the whole deck
         random.shuffle(actual_deck)
         return actual_deck
@@ -34,11 +32,13 @@ class Main():
     # User inputs # of decks but is converted to a string, ord converts string to integer
     temp_decks = input("Please enter the number of decks you would like to use: ")
     num_decks = int(temp_decks)
-    trial = deck()
+    trial = Deck()
     trial_deck = trial.create_deck(num_decks)
     trial.print_deck(trial_deck)
+    print('Your have been dealt a:')
     print(trial_deck.pop())
-    print(trial_deck.pop()) 
+    print('and a')
+    print(trial_deck.pop())
 
 
-main()
+Main()
