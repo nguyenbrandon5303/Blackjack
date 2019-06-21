03b71ba9936415121d39806ca4b0d0bb17f36a28
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2019-06-21 11:11:14 +0300

    Update main-new.py
    Deleted original code

--- main-new.py
+++ main-new.py
@@ -1,30 +1,49 @@
 
-# class Main():
 
-class Deck():
-    standard_deck = {"Ace of Spades": 11, "Ace of Hearts": 11, "Ace of Diamonds": 11, "Ace of Clubs": 11,
-            "King of Spades": 10, "King of Hearts": 10,"King of Diamonds": 10,"King of Clubs": 10,
-            "Queen of Spades": 10,"Queen of Hearts": 10,"Queen of Diamonds": 10,"Queen of Clubs": 10,
-            "Jack of Spades": 10,"Jack of Hearts": 10,"Jack of Diamonds": 10,"Jack of Clubs": 10,
-            "10 of Spades": 10,"10 of Hearts": 10,"10 of Diamonds": 10,"10 of Clubs": 10,
-            "9 of Spades": 9,"9 of Hearts": 9,"9 of Diamonds": 9,"9 of Clubs": 9,
-            "8 of Spades": 8,"8 of Hearts": 8,"8 of Diamonds": 8,"8 of Clubs": 8,
-            "7 of Spades": 7,"7 of Hearts": 7,"7 of Diamonds": 7,"7 of Clubs": 7,
-            "6 of Spades": 6,"6 of Hearts": 6,"6 of Diamonds": 6,"6 of Clubs": 6,
-            "5 of Spades": 5, "5 of Hearts": 5,"5 of Diamonds": 5,"5 of Clubs": 5,
-            "4 of Spades": 4,"4 of Hearts": 4,"4 of Diamonds": 4,"4 of Clubs": 4,
-            "3 of Spades": 3,"3 of Hearts": 3,"3 of Diamonds": 3,"3 of Clubs": 3,
-            "2 of Spades": 2,"2 of Hearts": 2,"2 of Diamonds": 2,"2 of Clubs": 2,
-            }
-    print(standard_deck )
-#     def deck_num:
-#
-#         pick how many decks you want
-#     def choosen_deck:
-#         standard_deck*deck_num
-# class Dealer():
-#
-# class Player():
-#
+# Blackjack game
+import random
 
-Deck()
+class deck():
+    #creates a deck with # of decks (deck_size) times 52
+    @staticmethod
+    def create_deck(deck_size):
+        
+        actual_deck = []
+        
+        for multi_deck in range(deck_size):
+            for single_deck in range(52):
+                actual_deck.append(single_deck+1)
+        
+        #shuffles the whole deck
+        random.shuffle(actual_deck)
+        return actual_deck
+    
+    #prints the whole deck
+    @staticmethod
+    def print_deck(actual_deck):
+        print(actual_deck)
+
+class Dealer():
+    #creates the dealer with condition of hitting until 17, standing otherwise
+
+# plays the game by going through each player + dealer, deals the cards, etc.
+class main():
+    print("Welcome to Blackjack")
+    
+    #User inputs # of decks but is converted to a string, ord converts string to integer
+    temp_decks = input("Please enter the number of decks you would like to use: ")
+    num_decks = int(temp_decks)
+    trial = deck()
+    trial_deck = trial.create_deck(num_decks)
+    trial.print_deck(trial_deck)
+    print(trial_deck.pop())
+    print(trial_deck.pop())
+
+
+main()
+
+
+
+
+
+
